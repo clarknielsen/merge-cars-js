ba1cb066b573775cae8332b603c8870ae2c5d5c0
Clark Nielsen <contact@clarknielsen.com>
2021-01-04 15:31:44 -0800

    init

---
+++ js/level.js
@@ -0,0 +1,80 @@
+class Level {
+  constructor() {
+    this.textureLoader = new THREE.TextureLoader();
+    this.modelLoader = new THREE.FBXLoader();
+    this.clock = new THREE.Clock();
+    this.deltaTime = this.clock.getDelta();
+
+    this.init();
+  }
+  init() {
+    this.loadTextures();
+    this.loadModels();
+  }
+  loadModel(modelName, path){
+    this.modelLoader.load( path, ( fbx ) =>{
+      this[`${modelName}`] = fbx;
+      this.loadedModels.push(fbx);
+    });
+  }
+  loadModels(){
+    this.loadedModels = [];
+    this.models = [
+        {name: 'level', path: 'models/level.fbx'},
+        {name: 'carPolice', path: 'models/car_police.fbx'}
+      ];
+    this.loadedModels.push = (model) => {
+      this.loadedModels[this.loadedModels.length] = model;
+      if(this.loadedModels.length === this.models.length){
+        this.buildScene();
+      }
+    };
+    for(let i = 0; i < this.models.length; i += 1){
+      this.loadModel(this.models[i].name, this.models[i].path);
+    }
+  }
+  loadTextures(){
+    this.textureLoader = new THREE.TextureLoader();
+    const promise = Promise.all([
+        this.textureLoader.load('img/tex_water.jpg')
+        ], (resolve, reject) => {
+      resolve(promise);
+    }).then(result => {
+      this.textures = result;
+    });
+  }
+  buildScene() {
+    this.buildWater();
+    this.addLevel();
+    this.setCamera();
+    this.update();
+  }
+  addLevel(){
+    scene.scene3D.add(this.level);
+    this.level.scale.set(0.005,0.005,0.005);
+  }
+  buildWater() {
+    this.waterTexture = this.textures[0]
+    this.waterTexture.wrapS = THREE.RepeatWrapping;
+    this.waterTexture.wrapT = THREE.RepeatWrapping;
+    this.waterTexture.repeat.set(30, 30);
+
+    const geometry = new THREE.PlaneBufferGeometry(150, 150,1,1);
+    const material = new THREE.MeshBasicMaterial({ map: this.waterTexture  });
+    this.floor = new THREE.Mesh(geometry, material);
+    this.floor.rotation.set(THREE.Math.degToRad(-90), 0, 0);
+    this.floor.position.set(0, -10, 0);
+    scene.scene3D.add(this.floor);
+
+    geometry.dispose();
+    material.dispose();
+  }
+  setCamera() {
+    scene.camera.position.set(0, 10, 10);
+    scene.camera.lookAt(new THREE.Vector3(0, 0, 0)); // set the correct camera angle
+  }
+  update() {
+    requestAnimationFrame(this.update.bind(this));
+    this.deltaTime = this.clock.getDelta();
+  }
+}
